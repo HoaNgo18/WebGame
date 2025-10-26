6e4507373b6b79d0c612831c06e4f49501b69c7b
Hoa Ngo <ngotuanhoa68@gmail.com>
2025-10-26 19:36:27 +0700

    project setup with workspaces & dependencies

---
+++ shared/src/constants.js
@@ -0,0 +1,14 @@
+// Game Settings
+export const MAP_SIZE = 5000;
+
+// Game Timing
+export const TICK_RATE = 60;
+export const INTERPOLATION_DELAY = 100;
+
+// Ship Stats
+export const SHIP_RADIUS = 20;
+export const SHIP_MAX_LIVES = 5;
+export const SHIP_MAX_SPEED = 300;
+export const SHIP_ACCELERATION = 600;
+export const SHIP_DECELERATION = 300;
+export const SHIP_ROTATION_SPEED = 3.5;
---
+++ shared/src/packetTypes.js
@@ -0,0 +1,11 @@
+export const PacketType = {
+    // Client -> Server
+    JOIN: 'join',
+    INPUT: 'input',
+
+    // Server -> Client
+    INIT: 'init',
+    UPDATE: 'update',
+    PLAYER_JOIN: 'player_join',
+    PLAYER_LEAVE: 'player_leave',
+};
